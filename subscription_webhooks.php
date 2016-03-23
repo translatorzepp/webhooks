1794be7fab0c8d500499bc84eb15e34820f222f5
zpalmer <zoepalmer2014@u.northwestern.edu>
2016-03-23 16:10:14 -0500

    Stripped out extraneous code testing the write-to-log-file, edited email contents to include parsed webhook info

--- subscription_webhooks.php
+++ subscription_webhooks.php
@@ -1,8 +1,5 @@
 <?php
 
-$message = "subscription_webhooks is running\n";
-file_put_contents("/var/www/html/webhooks/webhook.log", $message, FILE_APPEND);
-
 require_once "/var/www/braintree-php-3.9.0/lib/Braintree.php";
 
 date_default_timezone_set('America/Chicago');
@@ -14,31 +11,24 @@ Braintree_Configuration::privateKey('7dd7253c4c53d675f15e869212659579');
 
 if(isset($_POST["bt_signature"]) && isset($_POST["bt_payload"])) {
   
-    $to = 'zoe.palmer+webhooks@braintreepayments.com';
-    $subject = 'Webhook Notification';
-    $message = 'You got a webhook!';
-    $headers = 'From: zoe.palmer@braintreepayments.com' . "\r\n";
-
-    mail($to, $subject, $message, $headers);
-
-    $message = "Webhook Recieved";
-    file_put_contents("/var/www/html/webhooks/webhook.log", $message, FILE_APPEND);
-
     $webhookNotification = Braintree_WebhookNotification::parse(
         $_POST["bt_signature"], $_POST["bt_payload"]
     );
 
-    $message = " and Parsed";
-    file_put_contents("/var/www/html/webhooks/webhook.log", $message, FILE_APPEND);
+    $to = 'zoe.palmer+webhooks@braintreepayments.com';
+    $subject = 'Webhook Notification';
+    $headers = 'From: zoe.palmer@braintreepayments.com' . "\r\n";
 
-    if ($webhookNotification->kind == 'check') {
-        // $message = " " . $webhookNotification->timestamp->format('Y-m-d H:i:s') . " Kind: " . $webhookNotification->kind . " | url check successful\n";
-        $message = " Check\n";
-    }
-    else {
-        // $message = " " . $webhookNotification->timestamp->format('Y-m-d H:i:s') . " Kind: " . $webhookNotification->kind . " | " . "Subscription: " . $webhookNotification->subscription->id . "\n";
-        $message = " Subscription\n";
-    }
+    // if ($webhookNotification->kind == 'check') {
+    //     // $message = " " . $webhookNotification->timestamp->format('Y-m-d H:i:s') . " Kind: " .  . " | url check successful\n";
+    // }
+    // else {
+    //     // $message = " " . $webhookNotification->timestamp->format('Y-m-d H:i:s') . " Kind: " . $webhookNotification->kind . " | " . "Subscription: " . $webhookNotification->subscription->id . "\n";
+    // }
+    
+    $message = 'Webhook Received! ' . $webhookNotification->timestamp->format('Y-m-d H:i:s') . " Kind: " . $webhookNotification->kind;
+
+    mail($to, $subject, $message, $headers);
 
     file_put_contents("/var/www/html/webhooks/webhook.log", $message, FILE_APPEND);
 }
